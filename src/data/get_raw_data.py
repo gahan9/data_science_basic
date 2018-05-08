70d33e65e256ac269fe42e70dcb7f0c4216956ce
gahan9 <gahansaraiya@gmail.com>
2018-05-08 15:13:56 +0530

    enabled logging

--- src/data/get_raw_data.py
+++ src/data/get_raw_data.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv, find_dotenv
 from requests import session
 import logging
 
+__author__ = "Gahan Saraiya"
+__all__ = ['TitanicDisaster']
+
 
 class TitanicDisaster(object):
     def __init__(self):
@@ -13,34 +16,39 @@ class TitanicDisaster(object):
             'password': os.environ.get("KAGGLE_PASSWORD"),  # kaggle password
         }
         self.project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+        # set the path of the raw data
+        self.raw_data_path = os.path.join(self.project_dir, 'data', 'raw')
+        self.train_data_path = os.path.join(self.raw_data_path, 'train.csv')
+        self.test_data_path = os.path.join(self.raw_data_path, 'test.csv')
         self.log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         logging.basicConfig(level=logging.INFO, format=self.log_fmt)
         self.logger = logging.getLogger(__name__)
 
     def extract_data(self, url, file_path):
-        with session() as c:
-            c.post(self.kaggle_login_url, data=self.payload)
-            with open(file_path, 'wb') as handle:
-                response = c.get(url, stream=True)
-                for block in response.iter_content(1024):
-                    handle.write(block)
+        with session() as c:  # start session
+            c.post(self.kaggle_login_url, data=self.payload)  # login to kaggle with credential
+            with open(file_path, 'wb') as handle:  # open file in write-bytes mode
+                response = c.get(url, stream=True)  # get content as stream
+                for block in response.iter_content(1024):  # iterate over content
+                    handle.write(block)  # write bytes to file
 
-    def main(self):
+    def get_raw_data(self):
         self.logger.info("getting raw data")
+        # urls to get data
         train_url = "https://www.kaggle.com/c/3136/download/train.csv"
         test_url = "https://www.kaggle.com/c/3136/download/test.csv"
 
-        raw_data_path = os.path.join(self.project_dir, 'data', 'raw')
-        train_data_path = os.path.join(raw_data_path, 'train.csv')
-        test_data_path = os.path.join(raw_data_path, 'test.csv')
-
-        self.extract_data(train_url, train_data_path)
-        self.extract_data(test_url, test_data_path)
+        # store data from url
+        self.extract_data(train_url, self.train_data_path)
+        self.extract_data(test_url, self.test_data_path)
         self.logger.info("downloaded raw training and test data")
+        return self.train_data_path, self.test_data_path  # return data paths
 
 
 if __name__ == "__main__":
-    dotenv_path = find_dotenv()
-    load_dotenv(dotenv_path)
+    dotenv_path = find_dotenv()  # find '.env'
+    load_dotenv(dotenv_path)  # load '.env'
+
     titanic_obj = TitanicDisaster()
-    titanic_obj.main()
+    train_data, test_data = titanic_obj.get_raw_data()
+    titanic_obj
